3da836e51c7ea2523cb475a02b5d612533346185
greygooseoo <greygooseoeo@gmail.com>
2022-04-27 10:08:44 +0200

    implantation projet

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/icon-left-font.svg" class="logo">
+    <h1>Bienvenue sur votre solution de communication interne</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+<style>
+.logo{
+  width: 20%;
+}
+</style>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>Création du compte</h1>
+    <label for="name">Nom d'utilisateur</label><br>
+    <input type="text" id="name" name="name" required maxlength="30"><br>
+    <label for="mail">Adresse email</label><br>
+    <input type="text" id="mail" name="mail" required maxlength="30"><br>
+    <label for="password">Mot de passe</label><br>
+    <input type="text" id="password" name="password" requiredmaxlength="30"><br>
+    <label for="confPassword">Confirmer le Mot de passe</label><br>
+    <input type="text" id="confPassword" name="confPassword" requiredmaxlength="30"><br>
+    <b-input-group
+    v-for="size in ['sm','','lg']"
+    :key="size"
+    :size="size"
+    class="mb-3"
+    prepend="Label"
+  >
+    <b-form-input></b-form-input>
+    <b-input-group-append>
+      <b-button size="sm" text="Button" variant="success">Button</b-button>
+    </b-input-group-append>
+  </b-input-group>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
