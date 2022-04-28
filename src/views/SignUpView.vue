a445737a6aecdcc561a7ba7f9ee11503b662729e
greygooseoo <greygooseoeo@gmail.com>
2022-04-28 21:22:59 +0200

    ajout bouton et icone

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -24,14 +24,16 @@
         <b-input-group-prepend is-text>
           Mail
         </b-input-group-prepend>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <b-form-input type="email" aria-label="mail"></b-form-input>
       </b-input-group>
       <b-input-group class="mb-2">
         <b-input-group-prepend is-text>
           Mot de passe
         </b-input-group-prepend>
-        <b-form-input type="password" aria-label="mdp"></b-form-input>
+        <b-form-input type="password" aria-label="mdp"></b-form-input><br>
+        <i class="fas fa-eye"></i>
       </b-input-group>
+      <b-button variant="outline-success" class="d-flex justify-content-center mx-auto">Valider</b-button>
   </div>
   <div class="col-md-3">
     photo
